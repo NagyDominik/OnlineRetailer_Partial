7e1d417ec6650becaa5c234ab62a30c82aaa5391
Péter Sebők <33090588+sebokpeter@users.noreply.github.com>
2020-05-13 13:57:59 +0200

    Added overrides to Customer.Equals and Customer.GetHashCode to help unit testinf

--- CustomerApi/Models/Customer.cs
+++ CustomerApi/Models/Customer.cs
@@ -12,5 +12,23 @@ namespace CustomerApi.Models
         public string ShippingAddress { get; set; }
         [DefaultValue(true)]
         public bool CreditStanding { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+
+            Customer other = (Customer)obj;
+
+            return this.Id == other.Id && string.Equals(this.Name, other.Name) && string.Equals(this.Email, other.Email) &&
+                    string.Equals(this.PhoneNumber, other.PhoneNumber) && string.Equals(this.BillingAddress, other.BillingAddress) &&
+                    string.Equals(this.ShippingAddress, other.ShippingAddress) && this.CreditStanding == other.CreditStanding;
+         }
+
+        public override int GetHashCode()
+        {
+            return new { Id, Name, Email, PhoneNumber, BillingAddress, ShippingAddress, CreditStanding }.GetHashCode();
+        }
     }
 }
