a606d5d97e9129549911bd1e8bafecd5f156c4e7
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2020-05-18 23:22:31 +0200

    Update Startup.cs

--- OrderApi/Startup.cs
+++ OrderApi/Startup.cs
@@ -43,12 +43,12 @@ namespace OrderApi
                 sqlConnectionSrting = Environment.GetEnvironmentVariable("SQLServer");
             }
 
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            _conf = builder.Build();
+            //var builder = new ConfigurationBuilder()
+            //    .SetBasePath(env.ContentRootPath)
+            //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            //    .AddEnvironmentVariables();
+            //_conf = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
